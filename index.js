5e55b33818dc1da1db2ecd2cb6a240aba6b180d9
Edmond Wang <644568491@qq.com>
2019-04-22 11:18:00 +0800

    init express-graphql

--- index.js
+++ index.js
@@ -1,74 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const {
-    ApolloServer,
-    gql
-} = require("apollo-server");
+    buildSchema
+} = require('graphql');
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [{
-        title: "Harry Potter and the Chamber of Secrets",
-        author: "J.K. Rowling"
-    },
-    {
-        title: "Jurassic Park",
-        author: "Michael Crichton"
-    }
-];
-
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
-// Schema Definition Language (SDL), express the relatioships and shape of data.
-// where data come from and how it might be stored is out of scope of SDL.
-const typeDefs = gql `
-    # Comments in GraphQL are defined with the hash (#) symbol.
-
-    # This "Book" type can be used in other type declarations.
-    type Book {
-        """
-        It describes a book, contains title and author
-        """
-        title: String
-        author: String
-    }
-
-    type Author {
-        name: String
-        books: [Book]
-    }
-    # The "Query" type is the root of all GraphQL queries.
-    # (A "Mutation" type will be covered later on.)
+const app = express();
+const MyGraphQLSchema = buildSchema(`
     type Query {
-        getBooks: [Book]
-        getAuthors: [Author]
+        auto(id: Int!): Auto
     }
+    type Auto {
 
-    type Mutation {
-        addBook(title: String, author: String): Book
     }
-`;
+`);
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
-const resolvers = {
-    Query: {
-        getBooks: () => books,
-        // getAuthors: () => books.map(book => book.author)
-    }
-};
+app.use('/graphql', graphqlHTTP({
+    schema: MyGraphQLSchema,
+    graphiql: true
+}));
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-});
-
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
-server.listen().then(({
-    url
-}) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+app.listen(4000);
