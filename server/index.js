9550804563e1fd982f73e194021ada4362450539
Edmond Wang <644568491@qq.com>
2019-04-25 10:19:53 +0800

    move index.js into server folder

--- server/index.js
+++ server/index.js
@@ -12,19 +12,21 @@ const USER_NAME = "edmond";
 const PWD = "12345678";
 const db = `mongodb://${USER_NAME}:${PWD}@localhost:27017/${DATABASE_NAME}`;
 
-mongoose
-    .connect(db, {
-        useCreateIndex: true,
-        useNewUrlParser: true
-    })
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
+// mongoose
+//     .connect(db, {
+//         useCreateIndex: true,
+//         useNewUrlParser: true
+//     })
+//     .then(() => console.log("MongoDB connected"))
+//     .catch(err => console.log(err));
 
-const { buildSchema } = require("graphql");
+const {
+    buildSchema
+} = require("graphql");
 
 const schema = buildSchema(`
     type Query {
-        autos(): Auto
+        autos: Auto
     }
     type Auto {
         make: String
